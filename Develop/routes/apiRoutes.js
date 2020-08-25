05bae72ccfe423096305af7e71d1ddd855619c44
William Flores <williamsffc@gmail.com>
2020-08-25 09:43:12 -0700

    adding files

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,18 @@
+var notesArray = require("../db/db.json");
+
+module.exports = function (app) {
+
+    app.get("/api/notes", function (req, res) {
+        res.json(notesArray);
+    });
+
+    //POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+
+    app.post("/api/notes", function (req, res) {
+        var saveNote = req.body;
+        console.log(saveNote);
+        characters.push(saveNote);
+        res.json(saveNote);
+    });
+
+};
---
+++ Develop/server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
+
+var app = express();
+var PORT = 3030;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
