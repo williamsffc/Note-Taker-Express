1aad5d4f22a694114b62e7f8c33f16237bdf96ab
William Flores <williamsffc@gmail.com>
2020-08-28 09:58:43 -0700

    updated

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -18,22 +18,12 @@ module.exports = function (app) {
 
         noteArray.push(saveNote);
         console.log(noteArray);
+        
         fs.writeFile("db/db.json", JSON.stringify(noteArray), (err) => {
             if (err) throw err;
             console.log("Saved")
         })
-
         res.json(noteArray);
-
-        // function writeToFile(fileName, data) {
-        //     fs.writeFile(fileName, data, err => {
-        //         if (err) {
-        //             console.log("Note was not saved!");
-        //         }
-        //         console.log("Saved");
-        
-        //     });
-        // }
     });
 
     // DELETE / api / notes /: id - Should receive a query parameter containing the id of a note to delete.This means you'll need to find a way to give each note a unique id when it's saved.In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
@@ -52,13 +42,12 @@ module.exports = function (app) {
 
         res.json(filterArr);   
         // re-write to array what's left of the notes
-        writeToFile("db/db.json", JSON.stringify(filterArr));
+        // writeToFile("db/db.json", JSON.stringify(filterArr));
 
-        function writeToFile(fileName, data) {
-            fs.writeFile(fileName, data, err => {
-                if (err) throw err
-            });
-        }
-        
+        fs.writeFile("db/db.json", JSON.stringify(noteArray), (err) => {
+            if (err) throw err;
+            console.log("Saved")
+        })
+        res.json(noteArray);
     });
 };
